004232407c9ac96be6fc3ab9f67ef9b8463d9616
linzhihao <linzhihao@kuaishou.com>
2020-09-08 10:02:06 +0800

    搭建项目骨架

---
+++ src/main/java/com/youngforcoding/lambda/TestMain.java
@@ -0,0 +1,11 @@
+package com.youngforcoding.lambda;
+
+/**
+ * @author linzhihao <linzhihao@kuaishou.com>
+ * Created on 2020-09-08
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("hello !");
+    }
+}
